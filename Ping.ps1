4f4bcc7ee7fa4eaf79c577e762fad17d939c9058
BitPool.ro <41180471+bitpoolro@users.noreply.github.com>
2019-04-30 19:08:36 -0400

    Add files via upload

---
+++ Ping.ps1
@@ -0,0 +1,11 @@
+﻿<#
+	© 2018  		: BitPool 
+	Script			: Ping Multiple servers provided in Text file IPList.txt
+	Pre-requisite 	: Create a Text File IPList.txt
+#>
+$IPNodes = Get-Content "C:\temp\IPList.txt"
+foreach ($IP in $IPNodes)
+
+{ if (Test-Connection -IPAddress $IP -Count 1 -ErrorAction SilentlyContinue)
+{ Add-Content c:\Temp\Up.txt "$IP" } 
+else{ Add-Content c:\Temp\Down.txt "$IP" } } 
